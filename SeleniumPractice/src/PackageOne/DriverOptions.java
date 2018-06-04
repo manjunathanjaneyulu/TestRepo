2bc5c9609bde0871a0a8ad22918b8e264589b094
manjunathanjaneyulu <39880295+manjunathanjaneyulu@users.noreply.github.com>
2018-06-04 06:18:09 +0530

    Update DriverOptions.java

--- SeleniumPractice/src/PackageOne/DriverOptions.java
+++ SeleniumPractice/src/PackageOne/DriverOptions.java
@@ -14,12 +14,12 @@ public class DriverOptions {
 		Thread.sleep(10000);
 		System.out.println(driver.getCurrentUrl()); //https://www.facebook.com/
 		//System.out.println(driver.getPageSource());
-		//System.out.println(driver.getTitle()); //Facebook – log in or sign up
-		System.out.println(driver.getWindowHandle()); //4294967297
-		System.out.println(driver.getWindowHandles()); //[4294967297]
-		System.out.println(driver.getClass()); //class org.openqa.selenium.firefox.FirefoxDriver
-		System.out.println(driver.equals(driver)); //false
-		System.out.println(driver.toString()); //FirefoxDriver: firefox on XP (1bd28006-77a3-468f-91e8-e42f2d84343f)
+		//System.out.println(driver.getTitle()); //Facebook â€“ log in or sign up
+		//System.out.println(driver.getWindowHandle()); //4294967297
+		//System.out.println(driver.getWindowHandles()); //[4294967297]
+		//System.out.println(driver.getClass()); //class org.openqa.selenium.firefox.FirefoxDriver
+		//System.out.println(driver.equals(driver)); //false
+		//System.out.println(driver.toString()); //FirefoxDriver: firefox on XP (1bd28006-77a3-468f-91e8-e42f2d84343f)
 		System.out.println(driver.hashCode()); //783191662
 		driver1.get("https://www.google.com");
 		driver.close();
